f24a72516ccf3366f4b44e30d372dc6c13d6de47
TeamTimCapstone <158231534+TeamTimCapstone@users.noreply.github.com>
2024-04-30 10:57:11 -0500

    Clean Web Scraper comments and code

--- web_scraper.py
+++ web_scraper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape(url):
-#    url = 'https://www.cnn.com/2024/03/15/politics/supreme-court-rules-that-public-officials-can-block-social-media-followers/index.html'
     res = requests.get(url)
     html_page = res.content
     soup = BeautifulSoup(html_page, 'html.parser')
@@ -27,10 +26,4 @@ def scrape(url):
             output_with_newlines += '{} '.format(t)
             output = ''.join(output_with_newlines.splitlines())
     
-    #try:
-     #   output = output[output.find('.')+1:]
-    #    output = output[:output.rfind('.')+1]
-    #except:
-     #   pass
-
     return output
